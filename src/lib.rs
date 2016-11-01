e7b666ebd0cd798db4b71497ee4d2221cc8918dd
Derek Kastner <dkastner@gmail.com>
2016-10-31 20:57:48 -0500

    Working rust build

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,27 @@
 #![feature(lang_items)]
+#![feature(asm)]
 #![no_std]
 
+//mod vidio;
+
+//use vidio::TTY;
+
 #[no_mangle]
-pub extern fn rust_main() {}
+pub extern fn main() {
+    let x = 1;
+    //let tty = TTY::
+    //wtty(b'H');
+    //wtty(b'E');
+    //wtty(b'L');
+    //wtty(b'L');
+    //wtty(b'O');
+    //wtty(b' ');
+    //wtty(b'W');
+    //wtty(b'O');
+    //wtty(b'R');
+    //wtty(b'L');
+    //wtty(b'D');
+}
 
 #[lang = "eh_personality"] extern fn eh_personality() {}
 #[lang = "panic_fmt"] extern fn panic_fmt() -> ! {loop{}}
--- src/long_mode_init.asm
+++ src/long_mode_init.asm
@@ -3,8 +3,8 @@ global long_mode_start
 section .text
 bits 64
 long_mode_start:
-    extern rust_main
-    call rust_main
+    extern main
+    call main
 
     ; print `OKAY` to screen
     mov rax, 0x2f592f412f4b2f4f
